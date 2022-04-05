d240ab3420c8210f61100a5e5b7f37ae5d4190c1
Build <jwbradley2003@gmail.com>
2022-04-04 20:19:15 -0400

    Specify recon

--- ScpDeathmatch/Patches/VisionControllerPatch.cs
+++ ScpDeathmatch/Patches/VisionControllerPatch.cs
@@ -22,7 +22,7 @@ namespace ScpDeathmatch.Patches
         private static bool Prefix(Scp939_VisionController __instance)
         {
             Player player = Player.Get(__instance._myVisuals939.Hub);
-            if (player == null || player.IsScp)
+            if (player == null || !Plugin.Instance.Config.CustomRoles.Recon.Check(player))
                 return true;
 
             foreach (Visuals939 enabledEffect in Visuals939.EnabledEffects)
