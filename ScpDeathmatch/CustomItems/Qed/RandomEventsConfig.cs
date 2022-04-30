4cd7b02fb617a3c6bc780fa86c817d5ad341609b
Build <jwbradley2003@gmail.com>
2022-04-30 06:59:24 -0400

    add dynamic weight bound

--- ScpDeathmatch/CustomItems/Qed/RandomEventsConfig.cs
+++ ScpDeathmatch/CustomItems/Qed/RandomEventsConfig.cs
@@ -151,7 +151,12 @@ namespace ScpDeathmatch.CustomItems.Qed
 
             float distance = Vector3.Distance(ev.Thrower.Position, ev.Grenade.transform.position);
             float evaluatedWeight = eventChanceCurve.Evaluate(distance);
-            List<IRandomEvent> randomEvents = selectableEvents.Where(randomEvent => Math.Abs(randomEvent.Weight - evaluatedWeight) < 0.5f).ToList();
+
+            float minimumWeight = selectableEvents.Min(x => x.Weight);
+            float maximumWeight = selectableEvents.Max(x => x.Weight);
+            float bounds = Math.Abs(maximumWeight - minimumWeight) / 4f;
+
+            List<IRandomEvent> randomEvents = selectableEvents.Where(randomEvent => Math.Abs(randomEvent.Weight - evaluatedWeight) < bounds).ToList();
             return randomEvents.IsEmpty() ? null : randomEvents[UnityEngine.Random.Range(0, randomEvents.Count)];
         }
     }
